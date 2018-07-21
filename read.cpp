3dcc538c664502e97677896447c4376043c469ad
Aleksas Pielikis <ant.kampo@gmail.com>
2018-07-21 15:14:27 +0300

    Add new adopted sample code.

--- read.cpp
+++ read.cpp
@@ -1,7 +1,30 @@
 #include "decodedBufferPlayback.hpp"
+#include "ffmpegReader.hpp"
 
 extern "C" {
     #include <stdio.h>
+}
+
+int driver = -1;
+ao_device* device = NULL;
+
+void init_playback(int bits, int channels, int sample_rate)
+{
+    ao_sample_format sample_format;
+    sample_format.channels = channels;
+    sample_format.rate = sample_rate;
+    sample_format.bits = bits;
+    sample_format.byte_format = AO_FMT_NATIVE;
+    sample_format.matrix = 0;
+
+    // To initalize libao for playback
+    device = ao_open_live(driver, &sample_format, NULL);
+}
+
+void play_playback(char * buffer, int bufferSize)
+{
+    // Send the buffer contents to the audio device
+    ao_play(device, buffer, bufferSize);
 }
 
 void die(const char* message)
@@ -17,8 +40,26 @@ int main(int argc, char* argv[])
     }
  
     const char* input_filename = argv[1];
+
+
+    ao_initialize(); 
+    driver = ao_default_driver_id();
  
-    audio_decode_example(input_filename); 
+    av_register_all();
+
+    audio_decode_example(input_filename, &init_playback, &play_playback); 
+/*
+    FFmpegFile file(input_filename);
+    unsigned char buffer[2400];
+
+    int frames;
+    file.info(frames);
+    for (int i = 1; i <= frames; i++)
+    {
+        file.decode(buffer, i, &init_playback, &play_playback);
+    }
+*/
+    ao_shutdown();
   
     fprintf(stdout, "Done playing. Exiting...");
  
